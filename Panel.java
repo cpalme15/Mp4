c5dcf494b817d3e38ba24c7747d9af6ce6c1a9a8
cpalme15 <cpalme15@emich.edu>
2017-03-10 17:07:16 -0500

    Add files via upload

--- Panel.java
+++ Panel.java
@@ -13,6 +13,10 @@ public class Panel extends JPanel implements ActionListener{
 	private static final long serialVersionUID = -1085549162291721622L;
 	private JButton btn = null;
 	static JTextField tf_Answer = null;
+	public JTextField ptcount=null;
+	public JTextField questionnum=null;
+	public int qnum=0;
+	public int pt=0;
 	private JTextArea ta=null;
 	private Font quesFont=null;
 	private Trivia t1=null;
@@ -38,6 +42,11 @@ public class Panel extends JPanel implements ActionListener{
 		tf_Answer=new JTextField(20);
 		north=new JPanel();
 		north.setLayout(new BorderLayout());
+		north.setLayout(new BorderLayout());
+		ptcount=new JTextField(8);
+		questionnum=new JTextField(5);
+		ptcount.setText("Points:"+pt);
+		questionnum.setText("Q# "+qnum);
 		t1=new Trivia("Who was the first president of the USA?","george washington",1);
 		t2=new Trivia("What is the capital of Morocco?","rabat",3);
 		t3=new Trivia("Diana Prince is the public persona of which fictional superhero?","wonder woman",5);
@@ -50,6 +59,11 @@ public class Panel extends JPanel implements ActionListener{
 		j5=new JLabel(t5.getQuestion());
 		Font quesFont = j1.getFont().deriveFont(Font.PLAIN, 30f);
 		Font longquesFont=j1.getFont().deriveFont(Font.PLAIN,25f);
+		Font numFont=j1.getFont().deriveFont(Font.PLAIN,23f);
+		ptcount.setBackground(Color.LIGHT_GRAY);
+		questionnum.setBackground(Color.lightGray);
+	    ptcount.setFont(numFont);
+	    questionnum.setFont(numFont);
 		j1.setFont(quesFont);
 		j2.setFont(quesFont);
 		j3.setFont(longquesFont);
@@ -81,8 +95,11 @@ public class Panel extends JPanel implements ActionListener{
 		this.setFocusable(true);
 		this.setLayout(new BorderLayout());
 		this.add(btn, BorderLayout.SOUTH);
+		this.add(north,BorderLayout.NORTH);
 		this.add(tf_Answer, BorderLayout.CENTER);
-		this.add(cards,BorderLayout.NORTH);
+		north.add(cards,BorderLayout.CENTER);
+		north.add(ptcount,BorderLayout.WEST);
+		north.add(questionnum,BorderLayout.EAST);
 		cards.add(card1,"Card1");
 		cards.add(card2,"Card2");
 		cards.add(card3,"Card3");
@@ -120,8 +137,9 @@ public class Panel extends JPanel implements ActionListener{
 	{
 		if(Trivia.rightanswer==true)
 		{
-			
+			pt++;
 			cardLayout.next(cards);
+			qnum++;
 			tf_Answer.setText("");
 		} 
 		
